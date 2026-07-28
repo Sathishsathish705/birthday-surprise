a1eaf4fd7939a93efc8ff6141e2f8832b766e3dd
Sathish <sathishsathish25244@gmail.com>
2026-07-28 08:38:11 +0530

    Fixed MemoryStory import

--- src/components/MemoryStory.jsx
+++ src/components/MemoryStory.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import "../styles/memoryStory.css";
+import "../styles/MemoryStory.css";
 
 export default function MemoryStory({ onFinish }) {
 
